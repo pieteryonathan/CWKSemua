f649fec9f25b9a4fe7353f3773313f2fb87e1bc5
Pieter Yonathan <pieteryonathan20@gmail.com>
2021-06-07 13:42:05 +0700

    nambah ui dikit

---
+++ CWKSemua/HomePage/HomePageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageViewController.swift
+//  CWKSemua
+//
+//  Created by Pieter Yonathan on 07/06/21.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+    @IBOutlet weak var FCTButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
