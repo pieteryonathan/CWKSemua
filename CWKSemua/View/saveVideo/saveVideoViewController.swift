199829838f12c0fe274801df6bee5ddb572d6717
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-06-09 14:31:32 +0700

    save Video from Training

---
+++ CWKSemua/View/saveVideo/saveVideoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  saveVideoViewController.swift
+//  CWKSemua
+//
+//  Created by Azka Kusuma on 09/06/21.
+//
+
+import UIKit
+
+class saveVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
