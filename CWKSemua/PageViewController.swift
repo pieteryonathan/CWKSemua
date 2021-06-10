ee97379ede9e008d16fcb1767f849cab1ee3686c
Pieter Yonathan <pieteryonathan20@gmail.com>
2021-06-10 17:06:52 +0700

    kasih navigation di sumarry

--- CWKSemua/PageViewController.swift
+++ CWKSemua/PageViewController.swift
@@ -43,12 +43,12 @@ class PageViewController: UIViewController, UIScrollViewDelegate {
         
         for i in 0...imageArray.count-1 {
            
-            var imageView = UIImageView()
+            let imageView = UIImageView()
             imageView.contentMode = .scaleAspectFit
             imageView.image = UIImage(named: imageArray[i])
             
-            var imageWidth = scrollView.frame.width
-            var imageHeight = scrollView.frame.height
+            let imageWidth = scrollView.frame.width
+            let imageHeight = scrollView.frame.height
             
             
             imageView.frame = CGRect(x: CGFloat(i)*imageWidth, y: 0, width: imageWidth, height: imageHeight)
