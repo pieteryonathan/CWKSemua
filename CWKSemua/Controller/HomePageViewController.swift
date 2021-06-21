5bfb6a6ff3ff4579c7b8c447d6ccfac27017f4ea
Pieter Yonathan <pieteryonathan20@gmail.com>
2021-06-21 13:58:46 +0700

    fix lock rotation

--- CWKSemua/Controller/HomePageViewController.swift
+++ CWKSemua/Controller/HomePageViewController.swift
@@ -13,6 +13,8 @@ class HomePageViewController: UIViewController {
     
     override func viewDidLoad() {
             super.viewDidLoad()
+        let value = UIInterfaceOrientation.portrait.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
         
         }
         
